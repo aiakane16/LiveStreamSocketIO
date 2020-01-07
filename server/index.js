77a9793758adf7a1b1f2095f3d59d331c8ce3ed7
Kristian Edd Nemenzo <kristiannemenzo@gmail.com>
2020-01-07 05:52:56 +0000

    live video using socket io

--- server/index.js
+++ server/index.js
@@ -1,48 +1,100 @@
-var socketIO = require('socket.io');
-var server = require('http').createServer().listen(7000, '0.0.0.0');
-var io = socketIO.listen(server,{
-    extraHeaders: {
-      'Content-Type' : "application/x-www-form-urlencoded"
-    }
-  });
-var ss = require('socket.io-stream');
-var path = require('path');
-var fs = require('fs');
- 
-// Super simple server:
-//  * One room only. 
-//  * We expect two people max. 
-//  * No error handling.
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var sockets = [];
 
-io.sockets.on('connection', function (client) {
-    console.log('new connection: ' + client.id);
+app.use(express.static('public'))
 
-    client.broadcast.emit('broadcast', client.id + ' has connected');
+server.listen(3000,function(){
+    console.log('server listening on port 3000')
+});
 
-    var stream = ss.createStream();
+io.on('connection',function(client){
+    console.log(client.id + ' connected');
 
-    fs.createReadStream('sample-video.mp4').pipe(stream);
-
-    ss(client).emit('video-stream', stream, {
-        message: 'stream file '
+    client.emit('add-users', {
+        users: sockets
     });
 
+    client.broadcast.emit('add-users', {
+        users: [client.id]
+    });
 
+    client.on('make-offer',function(data){
+        console.log('make-offer')
+        client.broadcast.emit('offer-made',{
+            offer : data.offer,
+            socket : client.id
+        });
+        
+    });
+
+    client.on('make-answer',function(data){
+        console.log('make-answer')
+        client.broadcast.emit('answer-made',{
+            socket : client.id,
+            answer : data.answer
+        });
+    });
+
+    client.on('disconnect', function () {
+        sockets.splice(sockets.indexOf(client.id), 1);
+        io.emit('remove-user', client.id);
+    });
     
-    // ss(client).on('video-stream', function(stream, data) {
+    sockets.push(client.id);
 
-    // });
-
-    // ss(client).on('profile-image', function(stream, data) {
-    //     var filename = path.basename(data.name);
-    //     stream.pipe(fs.createWriteStream(filename));
-    //   });
-
-
-    // client.on('chat', function (data) {
-    //     client.broadcast.emit('chat', data);
-    //     console.log(client.id + ' message: ' + JSON.stringify(data));
-    // });
-
-    
 });
+
+
+
+
+// var socketIO = require('socket.io');
+// var server = require('http').createServer().listen(7000, '0.0.0.0');
+// var io = socketIO.listen(server,{
+//     extraHeaders: {
+//       'Content-Type' : "application/x-www-form-urlencoded"
+//     }
+//   });
+// var ss = require('socket.io-stream');
+// var path = require('path');
+// var fs = require('fs');
+ 
+// // Super simple server:
+// //  * One room only. 
+// //  * We expect two people max. 
+// //  * No error handling.
+
+// io.sockets.on('connection', function (client) {
+//     console.log('new connection: ' + client.id);
+
+//     client.broadcast.emit('broadcast', client.id + ' has connected');
+
+//     var stream = ss.createStream();
+
+//     fs.createReadStream('sample-video.mp4').pipe(stream);
+
+//     ss(client).emit('video-stream', stream, {
+//         message: 'stream file '
+//     });
+
+
+    
+//     // ss(client).on('video-stream', function(stream, data) {
+
+//     // });
+
+//     // ss(client).on('profile-image', function(stream, data) {
+//     //     var filename = path.basename(data.name);
+//     //     stream.pipe(fs.createWriteStream(filename));
+//     //   });
+
+
+//     // client.on('chat', function (data) {
+//     //     client.broadcast.emit('chat', data);
+//     //     console.log(client.id + ' message: ' + JSON.stringify(data));
+//     // });
+
+    
+// });
